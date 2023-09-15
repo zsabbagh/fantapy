99dbb31bc18a9327f2d6346fe271449912f73c0a
zsabbagh <zassabbagh@gmail.com>
2023-09-15 17:22:26 +0200

    Add more

--- main.py
+++ main.py
@@ -5,27 +5,57 @@ import pandas as pd
 from pprint import pprint
 from src.querier import FPLQuerier
 
+def generate_top_players(fpl: FPLQuerier, container=st):
+    """
+    Generate table of top players
+    """
+    results = []
+    columns = ['Name', 'Team', 'Position', 'Price', 'Points', 'xGI']
+    # TODO: Filter on easy fixtures
+    for player in fpl.players:
+        pass
 
-def main():
-    # Initialize querier
-    fpl = FPLQuerier()
-    st.title("FPyL")
-    # TODO: Top X Players
-    # Search bar
-    # First div with a clickable table
-    players_sorted = sorted(fpl.players.keys(), key=lambda x: x[0])
-    st.header("Player KPI")
-    # Display the selected option
-    player = st.selectbox(
-        'Select a player',
-        players_sorted,
-    )
-    kpi = fpl.query_player_kpi(fpl.players[player])
+def generate_player_metrics(fpl: FPLQuerier, container=st, player=None):
+    """
+    Generate player metrics
+    """
+    stats = fpl.players[player]['stats']
+    goals, assists, xg, xa, xgi = (
+        float(stats['goals']),
+        float(stats['assists']),
+        float(stats['xg']),
+        float(stats['xa']),
+        float(stats['xgi']),
+    ) 
+    gi = goals + assists
+    col1, col2, col3 = container.columns(3)
+    dgoals = f"{str(round(goals - xg, 2))} ({xg} xG)" if goals != xg else None
+    dassists = f"{str(round(assists - xa, 2))} ({xa} xA)" if assists != xa else None
+    dxgi = f"{str(round(gi - xgi, 2))} ({xgi} xGI)" if gi != xgi else None
+    col1.metric(label='Goals', value=goals, delta=dgoals)
+    col2.metric(label='Assists', value=assists, delta=dassists)
+    col3.metric(label='xGI', value=xgi, delta=dxgi)
+    # TODO: Good pick on fixtures?
+    container.write("Fixtures")
+    cola, colb, colc, cold, cole = container.columns(5)
+    team_strength = fpl.players[player]['team']['strength']
+    fixtures = fpl.players[player]['team']['fixtures']
+    cola.metric(label=f"GW{fixtures[0]['gw']}", value=fixtures[0]['code'], delta=fixtures[0]['difficulty']-team_strength, delta_color="inverse")
+    colb.metric(label=f"GW{fixtures[1]['gw']}", value=fixtures[1]['code'], delta=fixtures[1]['difficulty']-team_strength, delta_color="inverse")
+    colc.metric(label=f"GW{fixtures[2]['gw']}", value=fixtures[2]['code'], delta=fixtures[2]['difficulty']-team_strength, delta_color="inverse")
+    cold.metric(label=f"GW{fixtures[3]['gw']}", value=fixtures[3]['code'], delta=fixtures[3]['difficulty']-team_strength, delta_color="inverse")
+    cole.metric(label=f"GW{fixtures[4]['gw']}", value=fixtures[4]['code'], delta=fixtures[4]['difficulty']-team_strength, delta_color="inverse")
+
+def generate_player_charts(fpl: FPLQuerier, player=None, container=st):
+    """
+    Generate player KPI
+    """
+    kpi = fpl.query_player_kpi(player)
+    # Generate figure
     fig = go.Figure()
-    x = list(map(lambda x: f"GW{x['round']}", kpi))
+    x = list(map(lambda x: f"GW{x['round']} ({x['opponent']})", kpi))
     gi = list(map(lambda x: x['gi'], kpi))
     xgi = list(map(lambda x: x['xgi'], kpi))
-    form = list(map(lambda x: x['form'], kpi))
     # form = list(map(lambda x: x['form'], kpi))
     fig.add_trace(go.Scatter(x=x, y=gi, mode='lines+markers', name='GI'))
     fig.add_trace(go.Scatter(x=x, y=xgi, mode='lines+markers', name='xGI'))
@@ -33,10 +63,29 @@ def main():
     fig.update_layout(
         xaxis_title='GW',
         yaxis_title='',
-        title='Dynamic Scatter Plot',
+        title='Goal Involvements / xGI',
         hovermode="closest",
     )
-    st.plotly_chart(fig)
+    fig.update_xaxes(tick0=1, dtick=1)
+    fig.update_yaxes(tick0=0, dtick=1, range=[0, round(max(gi + xgi)) + 1])
+    container.plotly_chart(fig)
+    # Generate figure for form/price
+
+def main():
+    # Initialize querier
+    fpl = FPLQuerier()
+    st.title("FPyL")
+    generate_top_players(fpl)
+    # Player KPI
+    st.header("Player KPI")
+    players_sorted = sorted(fpl.players.keys(), key=lambda x: x[0])
+    player = st.selectbox(
+        'Select a player',
+        players_sorted,
+        index=0
+    )
+    generate_player_metrics(fpl, player=player)
+    generate_player_charts(fpl, player=player)
 
 if __name__ == '__main__':
     main()
--- src/querier.py
+++ src/querier.py
@@ -18,36 +18,79 @@ class FPLQuerier:
         Constructor for FPLQuerier class
         """
         self.data = requests.get(FPLQuerier.FPL_GENERAL_URL).json()
-        self.teams = {}
-        self.players = {}
+        self.teams, self.players, self.player_stats = {}, {}, {}
+        self.fixtures = requests.get(FPLQuerier.FPL_FIXTURES_URL).json()
+        self.fixtures = list(filter(lambda x: not x['started'], self.fixtures))
         for team in self.data.get('teams', []):
-            self.teams[team['id']] = team
+            self.teams[team['id']] = {
+                'name': team['name'],
+                'strength': team['strength'],
+                'code': team['short_name'],
+                'fixtures': [],
+            }
+        for fixture in self.fixtures:
+            team_a = fixture.get('team_a', None)
+            team_a_diff = fixture.get('team_a_difficulty', None)
+            team_h = fixture.get('team_h', None)
+            team_h_diff = fixture.get('team_h_difficulty', None)
+            gw = fixture['event']
+            if team_a in self.teams:
+                self.teams[team_a]['fixtures'].append({
+                    'team': team_h,
+                    'code': self.teams[team_h]['code'],
+                    'difficulty': team_a_diff,
+                    'gw': gw,
+                })
+            if team_h in self.teams:
+                self.teams[team_h]['fixtures'].append({
+                    'team': team_a,
+                    'code': self.teams[team_a]['code'],
+                    'difficulty': team_h_diff,
+                    'gw': gw,
+                })
         for player in self.data.get('elements', []):
-            self.players[player['web_name']] = player['id']
+            id = player['id']
+            team = self.teams.get(player['team'], {})
+            self.players[player['web_name']] = {
+                'id': id,
+                'team': team,
+                'stats': {
+                    'goals': int(player['goals_scored']),
+                    'assists': int(player['assists']),
+                    'xg': float(player['expected_goals']),
+                    'xa': float(player['expected_assists']),
+                    'xgi': float(player['expected_goal_involvements']),
+                    'xgc': float(player['expected_goals_conceded']),
+                },
+            }
     
-    def query_player_kpi(self, player_id: int):
+    def query_player_kpi(self, player: str):
         """
         Query KPI data for a player
         """
+        player_id = self.players[player]['id']
         data = requests.get(FPLQuerier.FPL_ELEMENT_SUMMARY_URL.format(player_id)).json()
-        results = []
+        history = []
         for gw in data['history']:
             round, pts, bpts, cost = gw['round'], gw['total_points'], gw['bonus'], gw['value']
-            xg, xa, xgi, xgc = gw['expected_goals'], gw['expected_assists'], gw['expected_goal_involvements'], gw['expected_goals_conceded']
+            xg, xa, xgi, xgc = float(gw['expected_goals']), float(gw['expected_assists']), float(gw['expected_goal_involvements']), float(gw['expected_goals_conceded'])
             goals, assists = gw['goals_scored'], gw['assists']
             gi = goals + assists
             # TODO: Get opponent
             opponent = self.teams.get(gw['opponent_team'], {}).get('name', 'Unknown')
             # form = gw['form']
-            results.append({
+            history.append({
                 'round': round,
                 'points': pts,
+                'cost': cost,
+                'bonus': bpts,
                 'gi': gi,
                 'xgi': xgi,
                 'opponent': opponent,
                 'form': pts / round,
             })
-        return results
+        self.players[player]['history'] = history
+        return history
 
     def query_player_by_name(self, name=None, max_results=10):
         """
